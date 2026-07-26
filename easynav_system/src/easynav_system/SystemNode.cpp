b9bac59cd2cd845df58de288bb7719f77b7c7c73
Francisco Martín Rico <fmrico@gmail.com>
2026-07-26 09:39:51 +0200

    Rebase from 'upstream'

--- easynav_system/src/easynav_system/SystemNode.cpp
+++ easynav_system/src/easynav_system/SystemNode.cpp
@@ -1,21 +1,17 @@
 // Copyright 2025 Intelligent Robotics Lab
 //
 // This file is part of the project Easy Navigation (EasyNav in short)
-// licensed under the GNU General Public License v3.0.
-// See <http://www.gnu.org/licenses/> for details.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-// Easy Navigation program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program. If not, see <http://www.gnu.org/licenses/>.
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 /// \file
 /// \brief Implementation of the SystemNode class.
@@ -29,7 +25,7 @@
 #include "easynav_planner/PlannerNode.hpp"
 #include "easynav_sensors/SensorsNode.hpp"
 #include "easynav_common/YTSession.hpp"
-#include "easynav_common/types/PointPerception.hpp"
+#include "easynav_sensors/types/PointPerception.hpp"
 #include "easynav_common/RTTFBuffer.hpp"
 
 #include "easynav_system/SystemNode.hpp"
@@ -46,28 +42,15 @@ SystemNode::SystemNode(const rclcpp::NodeOptions & options)
 
   nav_state_ = std::make_shared<NavState>();
 
-  NavState::register_printer<PointPerceptions>(
-    [](const PointPerceptions & perceptions) {
-      std::ostringstream ret;
-      ret << "PointPerception " << perceptions.size() << " with:\n";
-      for (const auto & perception : perceptions) {
-        ret << "\t[" << static_cast<const void *>(perception.get()) << "] --> "
-            << perception->data.size() << " points in frame [" << perception->frame_id
-            << "] with ts " << perception->stamp.seconds() << "\n";
-      }
-      return ret.str();
-    });
-
-
   NavState::register_printer<nav_msgs::msg::Goals>(
     [](const nav_msgs::msg::Goals & goals) {
-      std::string ret = "Goals " + std::to_string(goals.goals.size()) + " with :\n";
+      std::ostringstream ret;
+      ret << "{ " << rclcpp::Time(goals.header.stamp).seconds() << " } Goals " <<
+        goals.goals.size() << " with :\n";
       for (const auto & goal : goals.goals) {
-        std::string p_str = "\t--> (" + std::to_string(goal.pose.position.x) + ", " +
-        std::to_string(goal.pose.position.y) + ")\n";
-        ret = ret + p_str;
+        ret << "\t--> (" << goal.pose.position.x << ", " << goal.pose.position.y << ")\n";
       }
-      return ret;
+      return ret.str();
     });
 
   controller_node_ = ControllerNode::make_shared();
@@ -81,6 +64,7 @@ SystemNode::SystemNode(const rclcpp::NodeOptions & options)
   TFInfo tf_info;
   declare_parameter<std::string>("tf_prefix", tf_info.tf_prefix);
   declare_parameter<std::string>("robot_frame", tf_info.robot_frame);
+  declare_parameter<std::string>("robot_footprint_frame", tf_info.robot_footprint_frame);
   declare_parameter<std::string>("odom_frame", tf_info.odom_frame);
   declare_parameter<std::string>("map_frame", tf_info.map_frame);
   declare_parameter<std::string>("world_frame", tf_info.world_frame);
@@ -110,6 +94,7 @@ SystemNode::on_configure(const rclcpp_lifecycle::State & state)
   TFInfo tf_info;
   get_parameter<bool>("use_cmd_vel_stamped", use_cmd_vel_stamped_);
   get_parameter("robot_frame", tf_info.robot_frame);
+  get_parameter("robot_footprint_frame", tf_info.robot_footprint_frame);
   get_parameter("odom_frame", tf_info.odom_frame);
   get_parameter("map_frame", tf_info.map_frame);
   get_parameter("world_frame", tf_info.world_frame);
@@ -119,10 +104,10 @@ SystemNode::on_configure(const rclcpp_lifecycle::State & state)
   RTTFBuffer::getInstance()->set_tf_info(tf_info);
   RCLCPP_INFO(
     get_logger(),
-      "EasyNav configured with TFInfo: prefix='%s', map='%s', odom='%s', robot='%s', world='%s'",
+      "EasyNav configured with TFInfo: prefix='%s', map='%s', odom='%s', robot='%s', footprint='%s', world='%s'",
     tf_info.tf_prefix.c_str(), tf_info.map_frame.c_str(),
     tf_info.odom_frame.c_str(), tf_info.robot_frame.c_str(),
-    tf_info.world_frame.c_str());
+    tf_info.robot_footprint_frame.c_str(), tf_info.world_frame.c_str());
 
   for (auto & system_node : get_system_nodes()) {
     RCLCPP_INFO(get_logger(), "Configuring [%s]", system_node.first.c_str());
@@ -262,8 +247,8 @@ SystemNode::system_cycle()
   maps_manager_node_->cycle(nav_state_);
   goal_manager_->update(*nav_state_);
 
-  rclcpp::Time goals_ts(goal_manager_->get_goals().header.stamp);
   rclcpp::Time planner_ts = planner_node_->get_last_execution_ts();
+  rclcpp::Time goals_ts(goal_manager_->get_goals().header.stamp, planner_ts.get_clock_type());
 
   planner_node_->cycle(nav_state_, planner_ts < goals_ts);
 
