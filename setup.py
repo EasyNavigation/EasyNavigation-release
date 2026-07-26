01e44e6c6e3c60a4d91a21649df4c48a132da627
Francisco Martín Rico <fmrico@gmail.com>
2026-07-26 09:40:08 +0200

    Rebase from 'debian/jazzy/easynav_tools'

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 package_name = 'easynav_tools'
 setup(
     name=package_name,
-    version='0.2.2',
+    version='0.4.0',
     packages=find_packages(
         include=[package_name, package_name + '.*'], exclude=['test', 'scripts']
     ),
@@ -21,7 +21,7 @@ setup(
     maintainer='Francisco Martín Rico',
     maintainer_email='fmrico@gmail.com',
     description='ROS 2 Navigation tools: TUI (Textual) + ros2cli commands for EasyNav.',
-    license='GPL-3.0-or-later',
+    license='Apache-2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
